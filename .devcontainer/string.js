c4fc9d74ea0398e3db92fde2d64f68640d29c0e0
ANKIT KUMAR MAURYA <ankit67maurya@gmail.com>
2025-06-17 05:39:18 +0000

    strings changes

---
+++ .devcontainer/string.js
@@ -0,0 +1,39 @@
+const name = "hitesh-ch"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newstring=gameName.substring(0,4)
+// console.log(newstring);
+
+// const anotherstring=gameName.slice(-4,-1)
+// console.log(anotherstring);
+
+// console.log(gameName.split())
+
+const newstringone=" hitesh "
+// console.log(newstringone);
+
+// console.log(newstringone.trim());
+
+const url ="https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace("hitesh","ankit"))
+
+console.log(url.includes("hitesh"));
+
+console.log(gameName.split("-"))
+
