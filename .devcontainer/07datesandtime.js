32936bde8006bb2e22fcaabbebf1bcef58d08d46
ANKIT KUMAR MAURYA <ankit67maurya@gmail.com>
2025-06-18 08:06:13 +0000

    time and date

---
+++ .devcontainer/07datesandtime.js
@@ -0,0 +1,21 @@
+
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate);
+
+
+let newdate= new Date(2023,0,25)    // in js month start from cnt 0
+console.log(newdate.toDateString());
+
+let timestamp= Date.now()
+console.log(timestamp);
+
+
+
+
