6a6e855a06b8106351d09d600b20a906d426ab2a
ANKIT KUMAR MAURYA <ankit67maurya@gmail.com>
2025-06-18 12:43:49 +0000

    arrays

---
+++ 02basics/01arrays.js
@@ -0,0 +1,37 @@
+const arr=[1,2,3,4,5]
+const arr1=["shakti","mukti","arth"]
+
+console.log(arr[0]);
+
+const myarr=new Array(1,2,3,4,5)
+
+//array methods 
+
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+
+arr.unshift(10)
+arr.shift()
+// console.log(arr);
+
+// slice ,splice
+
+console.log("A",arr);
+
+const mynewarr=arr.slice(1,3)
+console.log(mynewarr);
+
+console.log("B",arr);
+
+
+const arr5 = [1, 2, 3, 4, 5];
+const sliced = arr5.slice(1, 4); // from index 1 to 3
+console.log(sliced); // [2, 3, 4]
+console.log(arr5);    // original array remains [1, 2, 3, 4, 5]
+
+
+const arr4 = [1, 2, 3, 4, 5];
+const spliced = arr4.splice(1, 2); // remove 2 items from index 1
+console.log(spliced); // [2, 3]
+console.log(arr4);     // modified array: [1, 4, 5]
