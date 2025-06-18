9a2dc3f4c5e6c117654ff22b8443b08d11a41a12
ANKIT KUMAR MAURYA <ankit67maurya@gmail.com>
2025-06-18 07:36:27 +0000

    maths

---
+++ .devcontainer/nums_and_maths.js
@@ -0,0 +1,29 @@
+
+// maths operations 
+
+// console.log(Math.round(4.7));
+// console.log(Math.abs(-4));
+// console.log(Math.floor(3.4));
+// console.log(Math.ceil(7.6));
+// console.log(Math.min(4,7,8,6));
+// console.log(Math.max(4,6,8,9));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min =10
+const max= 20 
+
+console.log(Math.floor(Math.random()*(max-min +1))+min)
+
+
+
+
+
+
+
+
+
+
+
